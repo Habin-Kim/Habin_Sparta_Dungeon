a2980e46409048a665b1c28c1817e1bd954f9ce5
t2024-m0215 <t2024-m0215@t2024-m0215ui-MacBookAir.local>
2025-05-22 12:27:12 +0900

    feat : Player platform follow
    feat Player platform follow(플레이어가 움직이는 발판 위에서 따라가게 됨)

--- Habin_Sparta_Dungeon/Assets/Scripts/Platform/MovePlatform.cs
+++ Habin_Sparta_Dungeon/Assets/Scripts/Platform/MovePlatform.cs
@@ -1,29 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovePlatform : MonoBehaviour
 {
-    public enum MoveAxis { X, Z }
-    [SerializeField] private MoveAxis moveAxis = MoveAxis.X;
+    public enum MoveAxis { X, Z } // 발판이 움직일 축을 정의하는 열거형
+    [SerializeField] private MoveAxis moveAxis = MoveAxis.X; // 인스펙터에서 움직일 축을 설정
 
-    [SerializeField] private float moveDistance = 3f;
-    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float moveDistance = 3f; // 발판이 이동할 거리
+    [SerializeField] private float moveSpeed = 2f; // 발판이 이동하는 속도
 
-    private Vector3 _lastPosition;
-    private Vector3 _deltaMovement;
-    private Vector3 _startPos;
+    private Vector3 _lastPosition; // 이전 프레임의 발판 위치
+    private Vector3 _deltaMovement; // 현재 프레임에서 이전 프레임까지의 발판 이동량
+    private Vector3 _startPos; // 발판이 처음 시작할 때의 위치
     private Rigidbody _rigdbody;
 
     void Start()
     {
         _startPos = transform.position;
-        _lastPosition = _startPos;
+        _lastPosition = _startPos; // 이동량 계산을 위한 초기 위치 저장
         _rigdbody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
+        // 프레임 간 이동 거리 계산
         _deltaMovement = transform.position - _lastPosition;
         _lastPosition = transform.position;
 
@@ -37,7 +36,8 @@ public class MovePlatform : MonoBehaviour
 
         _rigdbody.MovePosition(target);
     }
-
+    
+    // 외부에서 발판 이동량을 가져올 수 있게 함
     public Vector3 GetPlatformDelta()
     {
         return _deltaMovement;
--- Habin_Sparta_Dungeon/Assets/Scripts/Player/PlayerPlatformFollow.cs
+++ Habin_Sparta_Dungeon/Assets/Scripts/Player/PlayerPlatformFollow.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerPlatformFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private MovePlatform currentPlatform; // 플레이어가 닿은 발판 저장
+
+    void OnCollisionStay(Collision collision)
     {
-        
+        // 닿은 오브젝트에 MovePlatform 컴포넌트가 있으면 저장
+        if (collision.gameObject.TryGetComponent(out MovePlatform platform))
+        {
+            currentPlatform = platform;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionExit(Collision collision)
     {
-        
+        // 현재 발판에서 떨어졌을 경우 null로 초기화
+        if (collision.gameObject.GetComponent<MovePlatform>() == currentPlatform)
+        {
+            currentPlatform = null;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // 현재 발판이 존재할 경우, 발판의 이동량만큼 플레이어 위치도 함께 이동
+        if (currentPlatform != null)
+        {
+            transform.position += currentPlatform.GetPlatformDelta();
+        }
     }
 }
